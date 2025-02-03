0b5c42fb0b5302d942aa3936c1f273ecf435c5b3
Shayem1 <shayemiam@gmail.com>
2025-02-03 16:46:32 +1100

    wtf

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import sys
 from PIL import Image, ImageTk
 import datetime
 import xlsxwriter
+import os
 from openpyxl import Workbook
 from openpyxl import load_workbook
 # importing packages
@@ -12,19 +13,43 @@ from openpyxl import load_workbook
 
 # function to save data
 def save_data():
+    global workbook, worksheet, wb, sheet
+    name = str(day_of_week)+" "+str(T[2])+"-"+str(T[1])+"-"+str(T[0])+".xlsx"
+    wb.close()
 
-    for value in IDlist:
-        sheet.cell(row,1).value = str(row)
-        sheet.cell(row,2).value = str(value)
-        row += 1
+    if os.path.exists(name):
+        os.remove(name)
+
+        workbook = xlsxwriter.Workbook(name)
+        worksheet = workbook.add_worksheet()
+        worksheet.write(0, 1, "Id")
+        worksheet.set_column("A:G", 20)
+        workbook.close()
+        row = 2
+        wb = load_workbook(name, data_only=True)
+        sheet = wb.active
+
+        for i in button_list:
+            id = i.cget("text")
+            sheet.cell(row,1).value = str(row)
+            sheet.cell(row,2).value = str(id)
+            row += 1
+
         wb.template = False
         wb.save(name)
-    
+
+    GUI.after(5000, save_data)
+
 
 # function to remove invalid ID's
 def remove_red():
+    global red_list, button_list
     for i in red_list:
         i.destroy()
+    
+    for i in red_list:
+        if i in button_list:
+            button_list.remove(i)
     selected_student.configure(text="Selected Student:\nNo one selected")
     del_selected_ID.configure(state=DISABLED)
 
@@ -34,6 +59,7 @@ def selected_id(id):
     del_selected_ID.configure(state=DISABLED)
     selected_student.configure(text="Selected Student:\nNo one selected")
     GUI.update_idletasks()
+
 
 
 # function to select ID
@@ -58,34 +84,42 @@ def data_validation(string):
 
 # scanner main entry
 def ID_entered(event):
-    global IDlist, StudID, index, button_list
+    global IDlist, StudID, button_list, id, index, wb, sheet, red_list
     # student ID is sent to be validated
-    validation = data_validation(input_id.get())
+    id = input_id.get()
+    validation = data_validation(id)
 
     #adds ID's
     color = validation[1]
     Tcolor = validation[2]
-    StudID = customtkinter.CTkButton(master=IDframe, text=input_id.get(), font=("arial",35), fg_color=color, text_color=Tcolor, corner_radius=10)
+    StudID = customtkinter.CTkButton(master=IDframe, text=id, font=("arial",35), fg_color=color, text_color=Tcolor, corner_radius=10)
     StudID.pack(pady=10)
     
-    index = index + 1
     #makes the scroll wheel to the bottom/new entries
     GUI.update_idletasks()
     canvas.yview_moveto(1)
 
     # stores ID's in a list
-    IDlist.append(input_id.get())
+    IDlist.append(id)
     button_list.append(StudID)
     StudID.configure(command=lambda button = StudID: selection(button))
-
-
+    
+    
+    sheet.cell(index,1).value = str(index)
+    sheet.cell(index,2).value = str(id)
+    wb.save(name)
+    
+    print(index,id)
     if validation[0] == False:
         red_list.append(StudID)
 
+    index += 1
+
     # resets the entry field
     input_id.delete(0, END)
-
     time_update()
+
+
 
 def time_update():
     T = time.localtime()
@@ -95,8 +129,6 @@ def time_update():
     today = "Today is:\n\n"+str(day_of_week)+" "+str(T[2])+"-"+str(T[1])+"-"+str(T[0])+"  "+str(T[3])+":"+str(minute)
     time_text.configure(text=today)
     GUI.after(10000, time_update)
-    save_data()
-
 
 # initialising the GUI
 GUI = customtkinter.CTk()
@@ -107,6 +139,12 @@ GUI. title("Student ID Scanner")
 GUI.grid_rowconfigure(1, weight=1, uniform="equal")
 GUI.grid_columnconfigure(2, weight=1, uniform="equal")
 customtkinter.set_appearance_mode("dark")
+
+IDlist = [] #temp variable
+index = 2
+button_list = []
+red_list = []
+first_run = True
 
 # initialising student ID output frame
 IDframe = customtkinter.CTkScrollableFrame(master= GUI, width= 300, corner_radius=30)
@@ -126,18 +164,29 @@ if str(minute) == 1:
 today = "Today is: "+str(day_of_week)+str(T[2])+"-"+str(T[1])+"-"+str(T[0])+"  "+str(T[3])+":"+str(minute)
 time_text = customtkinter.CTkLabel(master= option_frame, text=today, font=("arial",40))
 time_text.place(relx = 0.2, rely = 0.05)
-time_update()
 
-#save files setup
-name = str(day_of_week)+str(T[2])+"-"+str(T[1])+"-"+str(T[0])+".xlsx"
+#save data setup
+name = str(day_of_week)+" "+str(T[2])+"-"+str(T[1])+"-"+str(T[0])+".xlsx"
+
+
+#prevents overwriting data after a crash
+if os.path.exists(name):
+        GUI.destroy()
+
 workbook = xlsxwriter.Workbook(name)
+
 worksheet = workbook.add_worksheet()
+
 worksheet.write(0, 1, "Id")
 worksheet.set_column("A:G", 20)
-row = 2
 workbook.close()
+
+row = 2
 wb = load_workbook(name, data_only=True)
 sheet = wb.active
+
+time_update()
+save_data()
 
 # text setup
 studentid_text = customtkinter.CTkLabel(master= GUI, text="   Student ID   ", font=("arial",45), fg_color="#41229c", corner_radius=10)
@@ -158,13 +207,6 @@ del_selected_ID.place(relx = 0.07, rely =0.55)
 selected_student = customtkinter.CTkLabel(master=option_frame, text="Selected Student:\nNo one selected", font=("arial",35))
 selected_student.place(relx = 0.52, rely = 0.56)
 
-global index
-IDlist = [] #temp variable
-button_list = []
-red_list = []
-first_run = True
-index = -1
-
 #set up entry box
 input_id = customtkinter.CTkEntry(master=option_frame, placeholder_text = "Write ID here", font=("arial",35), width=300, height= 45)
 input_id.place(relx = 0.5, rely = 0.35)
@@ -174,8 +216,5 @@ input_id.bind("<Return>", ID_entered)
 
 #binds the curser to the entry field
 input_id.focus_force()
-
-
-
 
 GUI.mainloop()
